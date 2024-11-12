72be3e3f9b21e3c84aeaaa3c1caf57d6453cdf69
Tackygun1 <134003227+Tackygun1@users.noreply.github.com>
2024-11-12 21:51:33 +0000

    done

--- Herons.java
+++ Herons.java
@@ -1,1 +1,27 @@
+public class Herons{
 
+
+
+    public static double findRoot(double x){
+        double guess = 1.00;
+        double i = x/guess - guess;
+
+        while (i > .000000000001){
+            guess = (guess + (x/guess))/2;
+            i = (x/guess - guess)*(-1); 
+           
+        }
+
+        return guess;
+    }
+    public static void main(String[] args){
+        System.out.println("25: "+findRoot(25));
+        System.out.println("2: "+findRoot(2));
+        System.out.println("115: "+findRoot(115));
+
+        
+
+        
+
+    }
+}
